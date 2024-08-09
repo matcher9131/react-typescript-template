0d6af5489e0384b3bc96d01f21c3dc550f608a3a
matcher9131 <matcher9131cd@gmail.com>
2024-08-09 09:46:16 +0900

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vite + React + TypeScript + TailwindCSS + Recoil
+
+A template of Vite + React + TypeScript + TailwindCSS + Recoil
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = (): JSX.Element => {
+    return (
+        <div>
+            <span className="text-blue-500">Hello, world!</span>
+        </div>
+    );
+};
+
+export default App;
